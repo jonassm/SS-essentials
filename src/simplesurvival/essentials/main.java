ba798c4c9ff955c1e652c558827300fde31de941
Jonassm <jonas-morbjerg@live.dk>
2017-04-10 02:40:18 +0200

    First commit

---
+++ src/simplesurvival/essentials/main.java
@@ -0,0 +1,16 @@
+package simplesurvival.essentials;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin {
+
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+	
+    }	
+	
+}
